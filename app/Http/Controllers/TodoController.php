6fcdc5cfeab791f46f157c8ab10477cbd3737610
makoto tanji <aiunwind@gmail.com>
2022-02-11 21:54:13 +0900

    first commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+use App\Http\Requests\ClientRequest;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        // $items = Todo::all();
+        // ページネーションが動作しない時用に残しています。
+        $items = Todo::Paginate(4);
+        return view('index', ['items' => $items]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ClientRequest $request)
+    {
+        //
+        Todo::create($request->all());
+        return redirect('/todos');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ClientRequest $request, Todo $todo)
+    {
+        //
+        $todo->content = $request->content;
+        $todo->save();
+        return redirect('/todos');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        //
+        $todo->delete();
+        return redirect('/todos');
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>実践ウェブアプリ</title>
+  <!-- 追加以下 -->
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/style.css"/>
+  <!-- 追加終了 -->
+</head>
+<body>
+  <div class="container flex">
+    <h2 class="ttl-todo">Todo List</h2>
+    <div class="main-container">
+      @if (count($errors) > 0)
+        <ul>
+          @foreach ($errors->all() as $error)
+          <li>{{$error}}</li>
+          @endforeach
+        </ul>
+      @endif
+      <form action="/todos" method="POST" class="form-add flex">
+        @csrf
+        <input type="text" name="content" class="input-add">
+        <input type="submit" class="btn btn-add" value="追加">
+      </form>
+      <!-- end store form -->
+
+      <table>
+        <tr>
+          <th>作成日</th>
+          <th>タスク名</th>
+          <th>更新</th>
+          <th>削除</th>
+        </tr>
+        @foreach ($items as $item)
+          <tr>
+            <td>{{$item->created_at}}</td>
+              <form action="/todos/{{$item->id}}" method="POST">
+                @method('PUT')
+                @csrf
+                <td>
+                  <input type="text" name="content" class="input_text" value="{{$item->content}}">
+                </td>
+                <td>
+                  <input type="submit" class="btn btn-update" value="更新">
+                </td>
+              </form>
+            <td>
+              <form action="/todos/{{$item->id}}" method="POST">
+                @method('DELETE')
+                @csrf
+                <input type="submit" class="btn btn-delete" value="削除">
+              </form>
+            </td>
+          </tr>
+        @endforeach
+      </table>
+      {{ $items->links() }}
+    </div>
+  </div>
+</body>
+</html>
